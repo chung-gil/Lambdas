d80e0f746217e6035743eeeeafbd089ba526f72b
chunggil han <changgilhan@gmail.com>
2021-07-11 12:11:42 -0400

    Add project files.

---
+++ BasicLambdas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BasicLambdas
+{
+    public delegate int MyDelegate(int x);
+    public delegate void MyDelegate2(int x, string prefix);
+    public delegate bool ExprDelegate(int x);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate foo = (x) => x * x;
+            Console.WriteLine($"The result of foo is: {foo(5)}");
+
+            foo = (x) => x * 10;
+            Console.WriteLine($"The result of foo is: {foo(5)}");
+
+            MyDelegate2 bar = (x, y) =>
+            {
+                Console.WriteLine($"The two-arg lambda {x * 10}, {y}");
+            };
+
+            bar(25, "Some string");
+
+            ExprDelegate baz = (x) => x > 10;
+            Console.WriteLine($"Calling baz with 5: {baz(5)}");
+            Console.WriteLine($"Calling baz with 15: {baz(15)}");
+        }
+    }
+}
---
+++ CodeChallenge/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace CodeChallenge
+{
+    public delegate void BalanceEventHandler(decimal theValue);
+
+    class PiggyBank
+    {
+        private decimal m_bankBalance;
+        public event BalanceEventHandler balanceChanged;
+
+        public decimal theBalance
+        {
+            set
+            {
+                m_bankBalance = value;
+                balanceChanged(value);
+            }
+            get
+            {
+                return m_bankBalance;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PiggyBank pb = new PiggyBank();
+
+            pb.balanceChanged += (amount) => Console.WriteLine($"The balance amount is {amount}");
+            pb.balanceChanged += (amount) => { if (amount > 500.0m) Console.WriteLine($"You reached your savings goal! You have {amount}"); };
+            
+            string theStr;
+            do
+            {
+                Console.WriteLine("How much to deposit?");
+
+                theStr = Console.ReadLine();
+                if(!theStr.Equals("exit"))
+                {
+                    if(decimal.TryParse(theStr, out decimal newVal))
+                    {
+                        pb.theBalance += newVal;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter valid number");
+                    }
+                }
+            }
+            while (!theStr.Equals("exit"));
+
+        }
+    }
+}
